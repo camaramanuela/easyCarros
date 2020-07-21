f10b47b15df19c6711e7a60eefc64f03c39d65c2
Manuela Bognar <manuelabcamara@gmail.com>
2020-07-21 12:32:39 -0300

    layout

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
+
+import logoImg from '../../assets/logo.png';
+import './styles.css';
+
 function Login() {
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+
   const history = useHistory();
 
   async function handleLogin(e) {
@@ -10,14 +17,33 @@ function Login() {
   } 
 
   return (
-    <div>
-      <p>Login</p>
+    <div className="login-container">
+    <section className="form">
 
-      <form onSubmit = { handleLogin } >      
-        <button type="submit">Entrar</button>
-      </form>
+        <form onSubmit = { handleLogin } >
+        
+          <img src={logoImg} alt="Easy Carros"/>
 
-    </div>
+          <input 
+            placeholder = "E-mail" 
+            type = "email"
+            value = {email} 
+            onChange = {e => setEmail(e.target.value) }
+          />
+
+          <input 
+            placeholder = "Senha" 
+            type = "password"
+            value = {password}
+            onChange = {e => setPassword(e.target.value) }
+          />
+
+          <button className="button" type="submit">Entrar</button>
+
+        </form>
+
+    </section>
+  </div>
   );
 }
 
